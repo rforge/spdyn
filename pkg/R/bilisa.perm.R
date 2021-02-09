08f18e05d0840b1d491669450175201c6b434fd3
osmarllq <osmarllq@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-02-09 02:16:50 +0000

    Added code pending executable code examples
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdyn@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bilisa.perm.R
+++ pkg/R/bilisa.perm.R
@@ -4,45 +4,50 @@
 # R script file 'Lisa Permutation Test.R'. The code on that scrpit forms the
 # basis to this function.
 
-bilisa.perm<-function(x,y,listw,perm=999,FUN=mean,...){
-
-zx<-x-FUN(x,...)
-zy<-y-FUN(y,...)
-n<-length(x)
-Sxy<-sqrt(sum(zx^2)/(n-1))*sqrt(sum(zy^2)/(n-1))
-lzy<-lag.listw(listw,zy)
-Ii<-(zx/Sxy)*lzy
-
-lisas<-matrix(0,n,perm)
-rids<-matrix(0,max(card(listw$neighbours)),perm)
-n_1<-n-1
-
-for(j in 1:perm){
-	rids[,j]<-sample.int(n_1,size=max(card(listw$neighbours)),replace=FALSE)
-    }
-
-for(i in 1:n){
-#	x=sample(zy[-i],replace=FALSE) 
-	x<-zy[-i]
-	rz<-x[as.numeric(rids[1:length(listw$weights[[i]]),])]
-	rz<-matrix(rz,nrow=length(listw$weights[[i]]),ncol=perm,byrow=FALSE)
-	lisas[i,]=zx[i]*(listw$weights[[i]]%*%rz)
-    }
-
-rlisas<-(1/Sxy)*lisas
-
-above<-rlisas>=Ii
-larger<-rowSums(above)
-low.extreme<-(perm-larger)<larger
-larger[low.extreme]<-perm-larger[low.extreme]
-
-p.sim<-(larger+1)/(perm+1)
-
-y<-cbind(Ii,p.sim,zx)
-colnames(y)<-c('Ii','p-val','Zi')
-class(y)<-'lisaPerm'
-return(y)
-	}
+bilisa.perm <- function(x,y=NULL,listw,perm=999,FUN=mean,DESV=sd, ...){
+  
+  if (is.null(y)) {
+    y <- as.numeric(x[, 2])
+    x <- as.numeric(x[, 1])
+  }
+  
+  zx <- x-FUN(x,...)
+  zy <- y-FUN(y,...)
+  n <- length(x)
+  Sxy <- DESV(x)*DESV(y)
+  lzy <- lag.listw(listw,zy)
+  Ii <- (zx/Sxy)*lzy
+  
+  lisas<-matrix(0,n,perm)
+  rids<-matrix(0,max(card(listw$neighbours)),perm)
+  n_1<-n-1
+  
+  for(j in 1:perm){
+    rids[,j]<-sample.int(n_1,size=max(card(listw$neighbours)),replace=FALSE)
+  }
+  
+  for(i in 1:n){
+    #	x=sample(zy[-i],replace=FALSE) 
+    x <- zy[-i]
+    rz <- x[as.numeric(rids[1:length(listw$weights[[i]]),])]
+    rz <- matrix(rz,nrow=length(listw$weights[[i]]),ncol=perm,byrow=FALSE)
+    lisas[i,] = zx[i]*(listw$weights[[i]]%*%rz)
+  }
+  
+  rlisas<-(1/Sxy)*lisas
+  
+  above<-rlisas>=Ii
+  larger<-rowSums(above)
+  low.extreme<-(perm-larger)<larger
+  larger[low.extreme]<-perm-larger[low.extreme]
+  
+  p.sim<-(larger+1)/(perm+1)
+  
+  y<-cbind(Ii,p.sim,zx)
+  colnames(y)<-c('Ii','p-val','Zi')
+  class(y)<-'lisaPerm'
+  return(y)
+}
 
 #save(lisa.perm,plot.lisaPerm,file='lisa.perm.q')
 
--- pkg/R/lisa.perm.R
+++ pkg/R/lisa.perm.R
@@ -4,43 +4,43 @@
 # R script file 'Lisa Permutation Test.R'. The code on that scrpit forms the
 # basis to this function.
 
-lisa.perm<-function(x,listw,perm=999,FUN=mean,...){
-
-z<-x-FUN(x,...)
-n<-length(x)
-s2<-sum(z^2)/(n-1)
-lz<-lag.listw(listw,z)
-Ii<-(z/s2)*lz
-
-lisas<-matrix(0,n,perm)
-rids<-matrix(0,max(card(listw$neighbours)),perm)
-n_1<-n-1
-
-for(j in 1:perm){
-	rids[,j]<-sample.int(n_1,size=max(card(listw$neighbours)),replace=FALSE)
-    }
-
-for(i in 1:n){
-#	x=sample(z[-i],replace=FALSE) 
-	x<-z[-i]
-	rz<-x[as.numeric(rids[1:length(listw$weights[[i]]),])]
-	rz<-matrix(rz,nrow=length(listw$weights[[i]]),ncol=perm,byrow=FALSE)
-	lisas[i,]=z[i]*(listw$weights[[i]]%*%rz)
-    }
-
-rlisas<-(1/s2)*lisas
-
-above<-rlisas>=Ii
-larger<-rowSums(above)
-low.extreme<-(perm-larger)<larger
-larger[low.extreme]<-perm-larger[low.extreme]
-
-p.sim<-(larger+1)/(perm+1)
-
-y<-cbind(Ii,p.sim,z)
-colnames(y)<-c('Ii','p-val','Zi')
-class(y)<-'lisaPerm'
-return(y)
-	}
+lisa.perm<-function(x,listw,perm=999,FUN=mean,DESV=sd,...){
+  
+  z<-x-FUN(x,...)
+  n<-length(x)
+  s2<-DESV(x)
+  lz<-lag.listw(listw,z)
+  Ii<-(z/s2)*lz
+  
+  lisas<-matrix(0,n,perm)
+  rids<-matrix(0,max(card(listw$neighbours)),perm)
+  n_1<-n-1
+  
+  for(j in 1:perm){
+    rids[,j]<-sample.int(n_1,size=max(card(listw$neighbours)),replace=FALSE)
+  }
+  
+  for(i in 1:n){
+    #	x=sample(z[-i],replace=FALSE) 
+    x <- z[-i]
+    rz <- x[as.numeric(rids[1:length(listw$weights[[i]]),])]
+    rz <- matrix(rz,nrow=length(listw$weights[[i]]),ncol=perm,byrow=FALSE)
+    lisas[i,] = z[i]*(listw$weights[[i]]%*%rz)
+  }
+  
+  rlisas<-(1/s2)*lisas
+  
+  above<-rlisas>=Ii
+  larger<-rowSums(above)
+  low.extreme<-(perm-larger)<larger
+  larger[low.extreme]<-perm-larger[low.extreme]
+  
+  p.sim<-(larger+1)/(perm+1)
+  
+  y<-cbind(Ii,p.sim,z)
+  colnames(y)<-c('Ii','p-val','Zi')
+  class(y)<-'lisaPerm'
+  return(y)
+}
 
 
